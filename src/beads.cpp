5ed9f606582a00d0f31201a5a820426e9f2b0f32
chenhongqiao <harrychen0314@gmail.com>
2020-06-11 10:03:05 -0700

    format beads

--- src/beads.cpp
+++ src/beads.cpp
@@ -3,41 +3,51 @@ ID: harrych3
 TASK: beads
 LANG: C++
 */
-#include<iostream>
+#include <iostream>
 using namespace std;
 char beads[400];
-int ans=0;
+int ans = 0;
 int main()
 {
     //decomment following lines when submit
     //freopen("beads.in","r",stdin);
     //freopen("beads.out","w",stdout);
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>beads[i];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> beads[i];
     }
-    for(int i=0;i<n;i++){
-        char c=beads[i];
-        int cnt=0;
-        for(int j=i;j<i+n;j++){
-            if(beads[j%n]==c||beads[j%n]=='w'){
+    for (int i = 0; i < n; i++)
+    {
+        char c = beads[i];
+        int cnt = 0;
+        for (int j = i; j < i + n; j++)
+        {
+            if (beads[j % n] == c || beads[j % n] == 'w')
+            {
                 cnt++;
-            } else{
+            }
+            else
+            {
                 break;
             }
         }
-        c=beads[i-1];
-        int rcnt=0;
-        for(int j=i-1;(j>=0&&j<i)||(j<0&&j%n>=i);j--){
-            if(beads[j%n]==c||beads[j%n]=='w'){
+        c = beads[i - 1];
+        int rcnt = 0;
+        for (int j = i - 1; (j >= 0 && j < i) || (j < 0 && j % n >= i); j--)
+        {
+            if (beads[j % n] == c || beads[j % n] == 'w')
+            {
                 rcnt++;
-            } else{
+            }
+            else
+            {
                 break;
             }
         }
-            ans=max(ans,cnt+rcnt);
+        ans = max(ans, cnt + rcnt);
     }
-    cout<<min(n,ans)<<endl;
+    cout << min(n, ans) << endl;
     return 0;
 }

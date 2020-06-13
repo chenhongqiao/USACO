ba23d03584b766cfe02b50a09d5475bb21af9363
Harry Chen <harrychen0314@gmail.com>
2020-06-13 11:55:43 -0700

    adopt setting for Fedora, start reorder

---
+++ src/reorder.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int a[105];
+int b[105];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+    }
+}

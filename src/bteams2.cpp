5cc3df68259b4cc2eca2f3233c3cf7644a04c3a3
Harry Chen <harrychen0314@gmail.com>
2020-06-30 15:21:02 -0700

    finish bteam

--- src/bteams2.cpp
+++ src/bteams2.cpp
@@ -5,8 +5,8 @@ int p[15];
 bool u[15];
 int plan[15];
 const int n = 12;
-int ans = 100000000;
 int cnt = 0;
+int ans = 100000000;
 void get_ans()
 {
     cnt++;
@@ -15,9 +15,10 @@ void get_ans()
     p2 = plan[3] + plan[4] + plan[5];
     p3 = plan[6] + plan[7] + plan[8];
     p4 = plan[9] + plan[10] + plan[11];
+
     ans = min(ans, max(p1, max(p2, max(p3, p4))) - min(p1, min(p2, min(p3, p4))));
 }
-void func(int dep, int l, int lm)
+void func(int dep, int l)
 {
     if (dep >= n)
     {
@@ -26,19 +27,15 @@ void func(int dep, int l, int lm)
     }
     if (dep % 3 == 0)
     {
-        l = lm;
+        l = 0;
     }
     for (int i = l; i < n; i++)
     {
         if (!u[i])
         {
-            if (dep % 3 == 0)
-            {
-                lm = i;
-            }
             u[i] = true;
             plan[dep] = p[i];
-            func(dep + 1, i, lm);
+            func(dep + 1, i);
             u[i] = false;
         }
     }
@@ -52,7 +49,7 @@ int main()
         cin >> p[i];
     }
     auto begin = chrono::high_resolution_clock::now();
-    func(0, 0, 0);
+    func(0, 0);
     auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(chrono::high_resolution_clock::now() - begin).count();
     cout << "time used=" << dur << " ms" << endl;
     cout << ans << " " << cnt << endl;

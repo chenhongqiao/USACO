36cfbcbee568149cb915272ca47208512e07404d
Harry Chen <harrychen0314@gmail.com>
2020-07-23 09:44:46 -0700

    chess

--- src/chess.cpp
+++ src/chess.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int n, k;
 char c[2005][2005];
 int fc[2005][2005];
-int sc[2005][2005];
 int fr[2005][2005];
-int sr[2005][2005];
 int main()
 {
     cin >> n >> k;
@@ -30,31 +28,6 @@ int main()
             }
         }
     }
-    for (int i = 0; i < n - k + 1; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j > 0)
-            {
-                sr[i][j] = sr[i][j - 1];
-            }
-            if (i > 0)
-            {
-                if (fr[j][i + k - 1] - fr[j][i - 1] == fr[j][n - 1])
-                {
-                    sr[i][j]++;
-                }
-            }
-            else
-            {
-                if (fr[j][i + k - 1] == fr[j][n - 1])
-                {
-                    sr[i][j]++;
-                }
-            }
-        }
-    }
-
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -69,42 +42,14 @@ int main()
             }
         }
     }
-    for (int i = 0; i < n - k + 1; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j > 0)
-            {
-                sc[i][j] = sc[i][j - 1];
-            }
-            if (i > 0)
-            {
-                if (fc[j][i + k - 1] - fc[j][i - 1] == fc[j][n - 1])
-                {
-                    sc[i][j]++;
-                }
-            }
-            else
-            {
-                if (fc[j][i + k - 1] == fc[j][n - 1])
-                {
-                    sc[i][j]++;
-                }
-            }
-        }
-    }
     int ans = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            int tmp1, tmp2;
-            if (i > 0)
-            {
-                tmp1 = sr[j][i]
-            }
+            ans = max(fc[i][j] + fr[j][i], ans);
         }
     }
-
+    cout << ans << endl;
     return 0;
 }

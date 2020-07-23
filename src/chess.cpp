f8965dc8bc457c8f64fceceb45449ab028943a78
Harry Chen <harrychen0314@gmail.com>
2020-07-23 12:56:45 -0700

    finish chess

--- src/chess.cpp
+++ src/chess.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 int n, k;
+int ow = 0;
 char c[2005][2005];
 int fc[2005][2005];
+int sc[2005][2005];
 int fr[2005][2005];
+int sr[2005][2005];
 int main()
 {
     cin >> n >> k;
@@ -14,6 +17,39 @@ int main()
             cin >> c[i][j];
         }
     }
+    for (int i = 0; i < n; i++)
+    {
+        bool w = true;
+        for (int j = 0; j < n; j++)
+        {
+            if (c[i][j] != 'W')
+            {
+                w = false;
+                break;
+            }
+        }
+        if (w)
+        {
+            ow++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        bool w = true;
+        for (int j = 0; j < n; j++)
+        {
+            if (c[j][i] != 'W')
+            {
+                w = false;
+                break;
+            }
+        }
+        if (w)
+        {
+            ow++;
+        }
+    }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -28,6 +64,31 @@ int main()
             }
         }
     }
+    for (int i = 0; i < n - k + 1; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j > 0)
+            {
+                sr[i][j] = sr[i][j - 1];
+            }
+            if (i > 0)
+            {
+                if (fr[j][i + k - 1] - fr[j][i - 1] == fr[j][n - 1] && fr[j][n - 1] != 0)
+                {
+                    sr[i][j]++;
+                }
+            }
+            else
+            {
+                if (fr[j][i + k - 1] == fr[j][n - 1] && fr[j][n - 1] != 0)
+                {
+                    sr[i][j]++;
+                }
+            }
+        }
+    }
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -42,14 +103,56 @@ int main()
             }
         }
     }
-    int ans = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - k + 1; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            ans = max(fc[i][j] + fr[j][i], ans);
+            if (j > 0)
+            {
+                sc[i][j] = sc[i][j - 1];
+            }
+            if (i > 0)
+            {
+                if (fc[j][i + k - 1] - fc[j][i - 1] == fc[j][n - 1] && fc[j][n - 1] != 0)
+                {
+                    sc[i][j]++;
+                }
+            }
+            else
+            {
+                if (fc[j][i + k - 1] == fc[j][n - 1] && fc[j][n - 1] != 0)
+                {
+                    sc[i][j]++;
+                }
+            }
         }
     }
-    cout << ans << endl;
+    int ans = 0;
+    for (int i = 0; i < n - k + 1; i++)
+    {
+        for (int j = 0; j < n - k + 1; j++)
+        {
+            int tmp1, tmp2;
+            if (i > 0)
+            {
+                tmp1 = sr[j][i + k - 1] - sr[j][i - 1];
+            }
+            else
+            {
+                tmp1 = sr[j][i + k - 1];
+            }
+            if (j > 0)
+            {
+                tmp2 = sc[i][j + k - 1] - sc[i][j - 1];
+            }
+            else
+            {
+                tmp2 = sc[i][j + k - 1];
+            }
+            ans = max(ans, tmp1 + tmp2);
+        }
+    }
+    cout << ans + ow << endl;
+
     return 0;
 }

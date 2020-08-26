29d7933873c856bea40be9947aed6c0dbfa99a1b
Harry Chen <harrychen0314@gmail.com>
2020-08-26 12:59:21 -0700

    finish several

---
+++ src/shuffle.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r[100005];
+bool v[100005];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> r[i];
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!v[i])
+        {
+            int c = i;
+            queue<int> q;
+            while (!v[c])
+            {
+                q.push(c);
+                v[c] = true;
+                c = r[c];
+            }
+            bool t = false;
+            while (!q.empty())
+            {
+                if (q.front() == c)
+                {
+                    t = true;
+                }
+                if (t)
+                {
+                    ans++;
+                }
+                q.pop();
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}

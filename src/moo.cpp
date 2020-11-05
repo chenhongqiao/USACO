24767097f908b409f7bcfb58b5102ef7e3c3131c
Harry Chen <harrychen0314@gmail.com>
2020-11-04 19:40:59 -0800

    finish moo

--- src/moo.cpp
+++ src/moo.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<bool, int>> s; //true->is mirror, then mi index; false->not mirror, then o number after this;
-vector<pair<int, int>> cov;
+vector<pair<bool, int>> s;  //true->is mirror, then tail; false->not mirror, then o number after this;
+vector<pair<int, int>> cov; //coverage; including head, not tail.
 int n;
+bool comp(int a, pair<int, int> b)
+{
+    return a < b.first;
+}
 void generate(int pos, int dep)
 {
-    if (pos > n)
+    if (pos > n + 10)
     {
         return;
     }
@@ -24,6 +28,36 @@ void generate(int pos, int dep)
     else
     {
         generate(pos + (dep + 3), dep + 1);
+    }
+}
+void solve(int h, int t, int m)
+{
+    int p = -100;
+    for (int i = h; i < t; i++)
+    {
+        if (cov[i].first <= m && m < cov[i].second)
+        {
+            p = i;
+            break;
+        }
+    }
+
+    if (!s[p].first)
+    {
+        if (cov[p].first == m)
+        {
+            cout << "m" << endl;
+            return;
+        }
+        else
+        {
+            cout << "o" << endl;
+            return;
+        }
+    }
+    else
+    {
+        solve(0, s[p].second, m - cov[p].first);
     }
 }
 void printmoo(int m)
@@ -54,5 +88,6 @@ int main()
     cin >> n;
     generate(0, 0);
     //print(0, s.size());
+    solve(0, s.size(), n - 1);
     return 0;
 }

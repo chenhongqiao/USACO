8c0d7cd6bbc04f180319b9119d4e9b605f0a6077
Harry Chen <harrychen0314@gmail.com>
2020-12-12 20:48:39 -0800

    finish pairup

---
+++ pairup/pairup.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> v;
+int main()
+{
+    freopen("pairup.in", "r", stdin);
+    freopen("pairup.out", "w", stdout);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({y, x});
+    }
+    sort(v.begin(), v.end());
+    int h = 0, t = v.size() - 1;
+    int ans = 0;
+    while (h <= t)
+    {
+        int tmp = min(v[h].second, v[t].second);
+        v[h].second -= tmp;
+        v[t].second -= tmp;
+        ans = max(ans, v[h].first + v[t].first);
+        if (v[h].second <= 0)
+        {
+            h++;
+        }
+        if (v[t].second <= 0)
+        {
+            t--;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}

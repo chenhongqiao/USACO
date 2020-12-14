c6601043baf37c0d9f58513a81fa45448e7a7b42
Harry Chen <harrychen0314@gmail.com>
2020-12-13 21:18:09 -0800

    update

--- lifeguards/lifeguards.cpp
+++ lifeguards/lifeguards.cpp
@@ -7,6 +7,8 @@ bool comp(const pair<int, int> a, const pair<int, int> b)
 }
 int main()
 {
+    //freopen("lifeguards.in", "r", stdin);
+    //freopen("lifeguards.out", "w", stdout);
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
